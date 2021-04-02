8443f46e42203ed52397927d39ebcf8efc520903
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2021-04-01 20:29:53 -0400

    fix(mongoose connector): mongoose is now using the unified topology when connecting to mongo

--- src/canto/src/mongoose.ts
+++ src/canto/src/mongoose.ts
@@ -3,13 +3,16 @@ import { Application } from './declarations';
 import logger from './logger';
 
 export default function (app: Application): void {
-  mongoose.connect(
-    app.get('mongodb'),
-    { useCreateIndex: true, useNewUrlParser: true }
-  ).catch(err => {
-    logger.error(err);
-    process.exit(1);
-  });
+  mongoose
+    .connect(app.get('mongodb'), {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((err) => {
+      logger.error(err);
+      process.exit(1);
+    });
 
   app.set('mongooseClient', mongoose);
 }
